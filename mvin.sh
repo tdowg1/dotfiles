e54bb8c7b6f89d512838542460f494b4a916a7c7
tdowg1 <tdowg1@a64eb3ba-58fa-4d19-8208-1a35c998e62f>
2010-02-27 03:00:04 +0000

    mvin: merge (reintegrate feature branch v0.2) r1027:1142 svn://svn/de/trunk/proj-branches/mvin/0.2-pull-push-diff
    git-svn-id: svn://svn/de/trunk/proj/mvin@1143 a64eb3ba-58fa-4d19-8208-1a35c998e62f

--- mvin.sh
+++ mvin.sh
@@ -1,195 +1,142 @@
 #!/bin/sh
 #
-# TODO
-# should be prompting user for each dotfile
-#	see how see tahoe installer handled default values
-#	see how other people are handling default values in their user promts
-# rename string: replace all WORK with WORKING
-#			^^tmp?  cause... it's really a temp directory...
-# give a thought to renaming all __foreach__ functions like...
-#	fforeachDotfile_COPY
+# this program is designed to assist in keeping your dotfiles portable and
+# synchronized.
 #
 #
+# $Id$
+# $HeadURL$
 #
+
+
+#
+# the only "safe" user-configurable option.
+# set CONF_FILE to where I can find the configuration file
+#===================================================================
+CONF_FILE=~/mvin.conf
+
+#===================================================================
+#
+# conf file sanity checking
+#===================================================================
+: ${CONF_FILE:?ERROR: CONF_FILE variable must be set}
+
 
 
 #
 # ARGUMENT PARSING AND SANITY CHECKING
 #===================================================================
 #
-
-if [ $USER = 'root' ] ; then
-	echo "no root user"
-	f_usage
-	exit 1;
-fi
-
-
 f_usage(){
+	local _me=$(basename ${0})
 	local _USAGE=$(cat <<__HEREDOC__
-${0}:
-designed to assist in keeping your dotfiles portable and
-synchronized. Default behavior is to pull dotfiles from remote
-server and put under home directory.
+Usage: ${_me} [OPTIONS] COMMAND
 
-USAGE: ${0} --[download|upload|shellrc] [--downloadonly]
-	--download
+List of commands:
+diff
+		compare local dotfiles with those from remote
+help
+		show this help message and exit
+pull
+		get dotfiles from remote
 		Probably the most common.
 		Download dotfiles from remote server (see mvin.conf) and put them under
 		your HOME.
 		Do this when you want to get the "latest" dotfile copy set. This will
 		obviously overwrite the dotfiles in your home directory.
-	--upload
-		Upload dotfiles from your HOME directory to remote server.
+push
+		send dotfiles to remote
+		Upload changed dotfiles from your HOME directory to remote server.
 		Reverses default "move in" behavior. Pushes or uploads
 		dotfiles (from home directory) to remote server.
 
 		Do this after you have: (1) performed a --download and subsequently
 		(2) updated dotfile(s) content and wish to OVERWRITE whatever is on
 		remote server.
-		|----> If you've forgotten what content is actually on the remote
+		|----> If youve forgotten what content is actually on the remote
 		server and wish to know whats about to be overwriten, see --downloadonly
 		option.
 
-	--shellrc=ACTION
-	ACTIONS:
-		add [--rc=FILE] --source=FILE
-			Adds a source statement to your shell environment profile (the --rc
-			argument) in order to have it automatically source a specified file
-			(the --source argument) upon your login.
-
-			Program assumes there will be a MAXIMUM of 1 source statement added
-			to a given shell environment profile.  Meaning, after executing this
-			with the add ACTION, before another add ACTION occurs, a remove ACTION
-			(below) occurs.
-		remove [--rc=FILE]
-			Opposite of add.
-			Tries to remove any source statement it previously inserted.
-
-		info
-**DISABLED**
-			get nfo about what this program knows about your shell environment such as
-				* your shell environment profile / rc file (~/.bashrc, ~/.zshrc)
-				* whether or not this program has modified said shellrc file and,
-				if it did, the contents.
-
-	--downloadonly
-**DISABLED**
-		Same as default behavior except dotfiles are not
-		put under home directory, but are left under the
-		"tmp.mvin" folder that this script uses as its
-		working directory.
-
-misc/notes:
-	lynx
-	wget
-	curl
+List of options:
+--force
+		sometimes pull or push operations will require this given certain
+		conditions
 __HEREDOC__
 )
-
 	echo "$_USAGE"
-	exit 1;
+	exit 1
 }
 
-# ensure at least 1 argument is given
-[ $# = 0 ] && f_usage
-
-#
-# source conf file, which should be sibling to curr script
-source "$(dirname $0)/mvin.conf"
 
 
-while [[ "$1" = -* ]] ; do
+if [ $USER = 'root' ] ; then
+	echo "ERROR: no root user"
+	f_usage
+fi
+if [ $# = 0 ] ; then
+	echo "ERROR: not sure what to do (no command given)"
+	f_usage
+fi
+
+
+
+while [[ $# != 0 ]] ; do
   case "$1" in
-    --download) # no-value arg
-			ACTION=download
-			shift
-			;;
-    --upload) # no-value arg
-			ACTION=upload
-#			isMvIn="NO"
-			shift
-			;;
-    --shellrc=?*) # specified like: --shellrc=FILE
-#			SHELLRCACTION=${1#--shellrc=}
-			ACTION="${1#--shellrc=}"
-			isShellrc="YES"
-			shift
-			;;
-    --rc=?*) # specified like: --rc=FILE
-    		SHELL_RC="${1#--rc=}"
+#    --shellrc=?*) # specified like: --shellrc=FILE
 
-    		if [ "$isShellrc" != "YES" ] ; then
-				echo "ERROR invalid argument $1"
-				f_usage
-			fi
-			shift
-			;;
-    --source=?*) # specified like: --source=FILE
-    		FILE_TO_SOURCE_WITHIN_SHELL_RC="${1#--source=}"
-
-    		if [ "$isShellrc" != "YES" ] ; then
-				echo "ERROR invalid argument $1"
-				f_usage
-			fi
-			shift
-			;;
-
-
-    --downloadonly) # no-value arg
-			isDownloadOnly="YES"
-			shift
-			;;
+    pull) COMMAND='PULL';;
+    push) COMMAND='PUSH';;
+    diff) COMMAND='DIFF';;
+    --force) ISFORCED='TRUE' ;;
+    --help|-h|help) f_usage ;;
     *)
-		echo "ERROR case matched wildcard"
+		echo "ERROR: unsupported COMMAND[$1] (case matched wildcard)"
 		f_usage
 		;;
   esac
+  shift
 done
 
 
 
 #
-# VARIABLE PARAMS/INITIALIZATION
+# VARIABLE PARAMS/INITIALIZATION AND SANITY CHECKING
 #===================================================================
 #
+#: ${cfparam__required_no:="example of cfparam=* where required=no"}
 # like cfparam=* required=yes
 : ${HOME:?ERROR: HOME variable must be set}
-: ${REMOTE_HOST_MVIN_ADDRESS:?ERROR: REMOTE_HOST_MVIN_ADDRESS variable must be set (see mvin.conf)}
-: ${ACTION:?ERROR: dont know what you want me to do (ACTION not set)}
-#if [ ${#ACTION} = 0 ] ; then
-#	echo ""
-#	f_usage
-#fi
+: ${COMMAND:?ERROR: dont know what you want me to do (COMMAND not set)}
+: ${ISFORCED:='FALSE'}
 
 
 #
-# like cfparam=* required=no
-#: ${isMvIn:="YES"}
-: ${isDownloadonly:="NO"}
-: ${isShellrc:="NO"}
-	#DEV-----
-	FILE_TO_SOURCE_WITHIN_SHELL_RC="$HOME/.mvin/dotfiles/.mvinrc"
+# source conf file, then sanity check variables that are expected to be
+# defined within the conf file
+if [ ! -f "$CONF_FILE" ] ; then
+	echo "ERROR: $CONF_FILE does not exist"
+	f_usage
+fi
 
+echo "using config file[$CONF_FILE]"
+source "$CONF_FILE"
+
+: ${SLEEPTIME:?ERROR: SLEEPTIME variable must be set}
+: ${REMOTE_HOST_MVIN_ADDRESS:?ERROR: REMOTE_HOST_MVIN_ADDRESS variable must be set}
+: ${dot_files:?ERROR: dot_files variable must be set}
+: ${dot_filesHOME:?ERROR: dot_filesHOME variable must be set}
+: ${MVINTMPPREFIX:?ERROR: MVINTMPPREFIX variable must be set}
+
+if [ ! -d "$MVINTMPPREFIX" ] ; then
+	echo "ERROR: $MVINTMPPREFIX does not exist"
+	f_usage
+fi
 
 #
-# the mvin program home directory
-: ${mvinHOME:="${HOME}/.mvin"}
-#
-# mvin's temp directory.  between when files are up/downloaded and
-# copied to the home directory, each one is copied here first.
-: ${mvinTMP:="${mvinHOME}/tmp"}
-
-#
-# remote server address
+# remote directories
+echo "using remote host [$REMOTE_HOST_MVIN_ADDRESS]"
 REMOTE_DOWNLOAD_DIRECTORY="${REMOTE_HOST_MVIN_ADDRESS}/dotfiles"
 REMOTE_UPLOAD_DIRECTORY="${REMOTE_HOST_MVIN_ADDRESS}/upload/"
-
-
-#
-# the set of all dot files to work with
-#: ${dot_files:="myaliases myvariables myfunctions"}
-: ${dot_files:=".inputrc .mvinrc .zshrc"}
 
 
 
@@ -198,40 +145,17 @@ REMOTE_UPLOAD_DIRECTORY="${REMOTE_HOST_MVIN_ADDRESS}/upload/"
 # FUNCTIONS
 #===================================================================
 #
-fCreateWorkingDirectory(){
-	if [ ! -d $mvinHOME -o ! -d $mvinTMP ] ; then
-		echo "creating $mvinTMP directory"
-		mkdir -p "${mvinTMP}"
-#DEVEL	else
-#DEVEL		echo "ERROR: $mvinHOME already exists! ...I want to be the one to create it"
-#DEVEL		exit 2
-	fi
-}
-
-
-fForeachDotfileDoDownload(){
-	remoteSourceDirectory="$1"
-	localDestinationDirectory="$2"
-
-	for dotfile in $dot_files ; do
-		fDownload "$remoteSourceDirectory/${dotfile}" "$localDestinationDirectory/${dotfile}"
-	done
-}
-
 fDownload(){
 	remoteSource="$1"
 	localDestination="$2"
-	wget --no-verbose --output-document="$localDestination" "$remoteSource"
+	
+	WGET_OPTIONS='' # very verbose
+	WGET_OPTIONS='--no-verbose' # less verbose
+	WGET_OPTIONS='--quiet' # silent; not even 404 errors
+	
+	wget $WGET_OPTIONS --output-document="$localDestination" "$remoteSource"
 }
 
-fForeachDotfileDoUpload(){
-	localSourceDirectory="$1"
-	remoteDestinationDirectory="$2"
-
-	for dotfile in $dot_files ; do
-		fUpload "$localSourceDirectory/${dotfile}" "$remoteDestinationDirectory"
-	done
-}
 
 fUpload(){
 	localSource="$1"
@@ -240,18 +164,61 @@ fUpload(){
 }
 
 
-fForeachDotfileDoCopy(){
-	sourceDirectory="$1"
-	destinationDirectory="$2"
-
-	for dotfile in $dot_files ; do
-		fCopy "$sourceDirectory/${dotfile}" "$destinationDirectory/${dotfile}"
-	done
-}
-
 fCopy(){
 	# simply a wrapper for cp
 	cp -v "$1" "$2"
+}
+
+
+fComputeDigest(){
+	md5sum "$1" | awk '{ print $1 }'
+}
+
+
+fPrintFileNfo(){
+	who=$1
+	metadata=$2
+	file=$3
+	
+	# give output like
+	#---> yours: $(md5sum FILE) /home/par/.mvin-myprofile
+	#---> theirs: $(md5sum FILE) /home/par/.mvin-myprofile
+	echo "---> $who $metadata $file"
+}
+
+
+fGetFileSize(){
+	stat --format=%b "$1"
+}
+
+
+fExecute_PULL(){
+	dotfile="$1"
+
+	cmd="fCopy $mvinTMP/${dotfile} $dot_filesHOME/${dotfile}"
+	echo "Executing[$cmd] in $SLEEPTIME seconds..."
+	sleep $SLEEPTIME
+	eval "$cmd"
+}
+
+
+fExecute_PUSH(){
+	dotfile="$1"
+	
+	cmd="fUpload $dot_filesHOME/${dotfile} $REMOTE_UPLOAD_DIRECTORY"
+	echo "Executing[$cmd] in $SLEEPTIME seconds..."
+	sleep $SLEEPTIME
+	echo "===== (web server response content block...)"
+	eval "$cmd"
+	echo "===== (... END web server response content)"
+}
+
+
+fExecute_DIFF(){
+	dotfile="$1"
+	cmd="diff $dot_filesHOME/${dotfile} $mvinTMP/${dotfile}"
+	echo "Executing[$cmd]"
+	eval "$cmd"
 }
 
 
@@ -261,62 +228,107 @@ fCopy(){
 # MAIN
 #===================================================================
 #
-SLEEPTIME=1
+mvinTMP="$(mktemp -d -p $MVINTMPPREFIX)"
+echo "using temp directory [$mvinTMP]"
 
-if [ "$ACTION" = "download" ] ; then
-	fCreateWorkingDirectory
-
-	echo "Retrieve dotfiles from remote host?... in $SLEEPTIME seconds..."
-	sleep $SLEEPTIME
-	fForeachDotfileDoDownload "$REMOTE_DOWNLOAD_DIRECTORY" "$mvinTMP"
+isForceRequired='TRUE'
 
 
-	echo "Copy dotfiles from $mvinTMP to HOME (files are overwritten"
-	echo "if they exist)? ...in $SLEEPTIME seconds..."
-	sleep $SLEEPTIME
-	fForeachDotfileDoCopy "$mvinTMP" "$HOME"
-
-
-elif [ "$ACTION" = "upload" ] ; then
-	echo "Copy dotfiles from HOME to $mvinTMP (files are overwritten"
-	echo "if they exist)? ...in $SLEEPTIME seconds..."
-	sleep $SLEEPTIME
-	fForeachDotfileDoCopy "$HOME" "$mvinTMP"
-
-
-	echo "Upload dotfiles to remote host?... in $SLEEPTIME seconds..."
-	sleep $SLEEPTIME
-	fForeachDotfileDoUpload "$mvinTMP" "$REMOTE_UPLOAD_DIRECTORY"
-fi
-
-
+for df in $dot_files ; do
+	echo "--> [$df]"
+	
+	
+	#
+	# get metadata on your dotfile
+	yourDf="$dot_filesHOME/${df}"
+	yourDfHash=
+	yourDfIsForceRequired='TRUE'
+	
+	if [ ! -f "$yourDf" ] ; then
+		yourFileNfo="DNE"
+		
+	elif [ "$( fGetFileSize "$yourDf" )" = 0 ] ; then
+		yourFileNfo="ZERO FILE SIZE"
+		
+	else
+		yourDfIsForceRequired='FALSE'
+		
+		yourFileNfo=$( fComputeDigest "$yourDf" )
+		yourDfHash="$yourFileNfo"
+	fi
+	
+	fPrintFileNfo " yours:" "$yourFileNfo" "$yourDf"
+	
+	
+	
+	#
+	# get metadata on their dotfile
+	fDownload "$REMOTE_DOWNLOAD_DIRECTORY/${df}" "$mvinTMP/${df}"
+	
+	theirDf="$mvinTMP/${df}"
+	theirDfHash=
+	theirDfIsForceRequired='TRUE'
+	
+	if [ ! -f "$theirDf" ] ; then
+		theirFileNfo="DNE"
+		#THIS WILL NEVER HAPPEN
+			echo "\n\nERRORR:somethings wrong with my logic-thought somehting's"
+			echo "would's never happen's but's its does\n\n\n"
+			exit 9
+		
+	elif [ "$( fGetFileSize "$theirDf" )" = 0 ] ; then
+		theirFileNfo="ZERO FILE SIZE"
+		
+	else
+		theirDfIsForceRequired='FALSE'
+		
+		theirFileNfo=$( fComputeDigest "$theirDf" )
+		theirDfHash="$theirFileNfo"
+	fi
+	
+	fPrintFileNfo "theirs:" "$theirFileNfo" "$theirDf"
+	
+	
+	#
+	# operate on dotfile
+	case "$COMMAND" in
+		PULL|PUSH)
+			#
+			# next few lines are just simplifying a bit of logic so that 1
+			# variable is used (instead of 2) during a later comparison
+			isForceRequired='FALSE'
+			[[ "$yourDfIsForceRequired" = "TRUE" ]] && isForceRequired='TRUE'
+			[[ "$theirDfIsForceRequired" = "TRUE" ]] && isForceRequired='TRUE'
+			
+			
+			# determine if operation should be performed or not
+			if [ "$yourDfHash" != "$theirDfHash" ] ; then
+				if [ "$isForceRequired" = "FALSE" \
+						-o \
+					"$isForceRequired" = "TRUE" -a "$ISFORCED" = "TRUE" ] ; then
+				
+					eval "fExecute_${COMMAND}" "$df"
+				else
+					echo "----> SKIPPED... (--force not specified)"
+				fi
+			fi ;;
+			
+		DIFF)
+			#
+			# only execute a diff when both files actually have content
+			# to be diffed... "when it makes sense" (i.e. if one file is empty, 
+			# it doesn't make sense to print out a diff)
+			if [ -n "$yourDfHash" -a -n "$theirDfHash"  \
+					-a \
+				"$yourDfHash" != "$theirDfHash" ] ; then
+				
+				eval "fExecute_${COMMAND}" "$df"
+			fi ;;
+		
+		*) echo "ERROR: unsupported COMMAND[$1] (case matched wildcard)" ;;
+	esac
+	
+done
 
 exit $?
-
-########### MAYBE LATER............
-
-#
-# PROFILE SOURCING
-#
-# the calling parent profile (i.e. .bashrc should source .mvinrc)
-PARENT_PROFILE=~/.bashrc
-MVIN_PROFILE_MVINRC='~/.mvinrc'
-
-#
-# check if calling parent profile needs to be "patched" (really updated) so that
-# it'll source .mvinrc (which in turn will source custom profiles)
-grep -n ". $MVIN_PROFILE_MVINRC" "$PARENT_PROFILE"
-
-if [ $? = 0 ] ; then
-	echo "$PARENT_PROFILE is patched; nothing to do"
-
-else
-	echo "Updating $PARENT_PROFILE to source custom profiles"
-
-	tmpSourcingProfile="[[ -f $MVIN_PROFILE_MVINRC ]] && . $MVIN_PROFILE_MVINRC"
-
-	echo "" >> $PARENT_PROFILE
-	echo "# mvin.sh adds sourcing code" >> $PARENT_PROFILE
-	echo "$tmpSourcingProfile" >> $PARENT_PROFILE
-fi
 
