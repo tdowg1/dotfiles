2b58c7f909b8cf8ba878cf6fa6c8de656d8a7fe1
Bryn Davies <tdowg1@yahoo.com>
2011-09-22 00:09:29 -0400

    merge-into-home MAJOR: merges together variables config
    MORE NFO
    * this commit noted as 'MAJOR' because .variables.sh is one of my Big Four
    env config files.

--- home-machines/.variables.sh
+++ home-machines/.variables.sh
@@ -1,38 +1,36 @@
 # .variables.sh
 
+## ### #### ###################################################################
+##
+## misc.
+##
+## ### #### ###################################################################
 
-update_PATH_with_teelah_utils=:/opt/teelah-utils/bin:/opt/teelah-utils/bin/sys-config:/opt/teelah-utils/bin/sys-config/trac:/opt/teelah-utils/bin/sys-config/svn:/opt/teelah-utils/bin/trac:/opt/teelah-utils/bin/muzik:/opt/teelah-utils/bin/svn:/opt/teelah-utils/bin/filename_transformations:/opt/teelah-utils/bin/filemanagement:/opt/teelah-utils/bin/custom:/opt/teelah-utils/bin/php:/opt/teelah-utils/bin/in-development:/opt/teelah-utils/bin/in-development/tmp
-export PATH=${PATH}:${update_PATH_with_teelah_utils}
-
-export PYTHON_EXTERNALS_PATH=/opt/python-externals
-export PYTHONPATH=/opt/teelah-utils/bin:$PYTHON_EXTERNALS_PATH
 export EDITOR=vim
 export SVN_EDITOR=vim
-
-
 
 
 ##
 ## FROM SVN.HOME MACHINE...
 export PATH=$PATH:/sbin:/usr/sbin
 
-# BASIX
-#not sure if need these...
-   #set -o emacs
-   #bind -f /etc/inputrc
-
-# PROGS
 export PAGER="less"
 
+# 2011-09-01:
+# src: http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+# Less Colors for Man Pages
+export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_me=$'\E[0m'           # end mode
+export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
+# disabled standout-mode bc very difficult to see.
+#export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\E[0m'           # end underline
+export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
+###SWEET!!! it works!
 
 
 
-
-
-
-##
-## teelah customs -- ------- VARIABLES ---------------
-## ### #### ###################################################################
 
 # 2011-07-09: tweaks for a git working directory (/etc/bash_completion.d/git)
 if [[ -f /etc/bash_completion.d/git ]] ; then
@@ -44,4 +42,126 @@ if [[ -f /etc/bash_completion.d/git ]] ; then
 	GIT_PS1_SHOWUNTRACKEDFILES=true
 	GIT_PS1_SHOWUPSTREAM="auto"
 fi
+
+
+
+
+
+
+## ### #### ###################################################################
+##
+## HOST-specific
+##
+## ### #### ###################################################################
+
+## 
+## PHISATA-specific
+if [[ x"${IS_I_ON_PHISATA}" = x"true" ]] ; then
+	update_PATH_with_teelah_utils=:/opt/teelah-utils/bin:/opt/teelah-utils/bin/sys-config:/opt/teelah-utils/bin/sys-config/trac:/opt/teelah-utils/bin/sys-config/svn:/opt/teelah-utils/bin/trac:/opt/teelah-utils/bin/muzik:/opt/teelah-utils/bin/svn:/opt/teelah-utils/bin/filename_transformations:/opt/teelah-utils/bin/filemanagement:/opt/teelah-utils/bin/custom:/opt/teelah-utils/bin/php:/opt/teelah-utils/bin/in-development:/opt/teelah-utils/bin/in-development/tmp
+	export PATH=${PATH}:${update_PATH_with_teelah_utils}
+
+	export PYTHON_EXTERNALS_PATH=/opt/python-externals
+	export PYTHONPATH=/opt/teelah-utils/bin:$PYTHON_EXTERNALS_PATH
+fi
+## /PHISATA-specific
+##
+
+
+
+##
+## ARINC-specific
+#
+#BDAVIESPC=bdavies522276
+## /ARINC-specific
+##
+
+
+
+##
+## [com.spryinc.]MAGNIFICENT-specific
+if [[ x"${IS_I_ON_MAGNIFICENT}" = x"true" ]] ; then
+
+	#
+	# 2011-06-08: NOTE JAVA_HOME already set in /etc/profile
+	#export JAVA_HOME=/opt/jdk1.6.0_25
+	# 2011-06-24: NOTE M2_HOME already set in /etc/profile
+	export M2_HOME=/usr/share/maven2
+	###
+	#^^^^CAN PROB RE-COMMENT THIS OUT AFTER AN OS BOUNCE???
+	##
+	## (AND WHEN DO THIS, remove maven config entered for IntelliJ
+	## for project gadget-hr-fed, and ensure IntelliJ still builds
+	## after removing)
+	##
+
+	PATH=$PATH:$HOME/bin:$HOME/opt/bin
+
+	# 2011-06-08: sbin's already set somewhere else! wtf?
+	#PATH=$PATH:/sbin:/usr/sbin
+
+	PATH=$JAVA_HOME/bin:$PATH
+	export PATH=$PATH
+
+
+	export HISTTIMEFORMAT="%F_%T"
+
+	#HISTCONTROL=ignoreboth
+	HISTCONTROL=ignoredups:ignorespace
+
+	# append to the history file, don't overwrite it
+	shopt -s histappend
+
+	# 2011-06-17: tweak bash history
+	# 2011-08-24: OK, IDK why these aren't working...
+	### trying without using bash math operators and without 'exporting'
+	### if /that/ doesn't even seem to work, maybe these are special? for 
+	### some reason?? =---maybe they ACTUALLY HAVE TO BE set in the .bashrc
+	### and/or /etc/bash.bashrc file?...
+	# 2011-08-25: ZOMFG: .bashrc (the default that was installed to my
+	### home by ubu, is diff than fedora's.  HIST*SIZE's are all set there!
+	### freak me.
+
+	#unset HISTSIZE  # the number of commands to save in a history list.
+	#export HISTSIZE=$(( 5000 * 20 ))
+	HISTSIZE=5000
+
+	# The maximum number of lines contained in the history file.
+	# If HISTFILESIZE is not set, no truncation is performed.
+	#unset HISTFILESIZE
+	#export HISTFILESIZE=$(( 10000 * 2 ))
+	HISTFILESIZE=10000
+
+
+
+
+	# 2011-06-23: seeing if can save screen sessions.
+	# 2011-09-21: ^^NO YOU CANT, BUT YOU CAN HAVE IT AUTOCREATE TABSS
+	# src: http://superuser.com/questions/117000/tell-gnu-screen-where-to-save-the-sessions
+	export SCREENDIR=$HOME/.screen
+
+	export TOMCAT_HOME=/opt/apache-tomcat-6.0.26/
+
+
+	##
+	## git-related
+
+	# 2011-06-24: git
+	# for nfo, see: /etc/bash.completion.d/git 
+	#PREV_PS1="$PS1"
+	#PS1='\u@\h \W$(__git_ps1 " (%s)")\$ '
+	#
+	## unstaged (*) and staged (+) changes will be shown next to the branch name
+	#GIT_PS1_SHOWDIRTYSTATE=true
+	#
+	## if something is stashed, a '$' will be shown next to the branch name
+	#GIT_PS1_SHOWSTASHSTATE=
+	#
+	## if there're untracked files, a '%' will be shown next to the branch name
+	#GIT_PS1_SHOWUNTRACKEDFILES=true
+	## /git-related
+	##
+
+fi # /IS_I_ON_MAGNIFICENT
+## /[com.spryinc.]MAGNIFICENT-specific
+##
 
