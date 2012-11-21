917fbc9d28b7e6c5ca4e4c92692d97cd24935d3e
Bryn Davies <bdavies@spryinc.com>
2012-11-21 16:36:33 -0500

    misc dotfile changes (shazam)

--- home-machines/.functions.sh
+++ home-machines/.functions.sh
@@ -1389,6 +1389,31 @@ $ git diff-tree -p HEAD           # INCORRECT get diff (in patch format) between
 $ git diff-tree -p HEAD           # get diff (in patch format) between HEAD^ and HEAD (and in that order)
 __envHEREDOC__
 }
+helpgit5(){
+      cat <<'__envHEREDOC__'
+Fix "non-bare" upstream repo.  For instance, may be receiving this when trying
+to push changes upstream:
+	http://stackoverflow.com/questions/2199897/how-to-convert-a-git-repository-from-normal-to-bare
+		remote: error: refusing to update checked out branch: refs/heads/master
+		remote: error: By default, updating the current branch in a non-bare repository
+		remote: error: is denied, because it will make the index and work tree inconsistent
+		remote: error: with what you pushed, and will require 'git reset --hard' to match
+		remote: error: the work tree to HEAD.
+		remote: error:
+		remote: error: You can set 'receive.denyCurrentBranch' configuration variable to
+		remote: error: 'ignore' or 'warn' in the remote repository to allow pushing into
+		remote: error: its current branch; however, this is not recommended unless you
+		remote: error: arranged to update its work tree to match what you pushed in some
+		remote: error: other way.
+		remote: error:
+		remote: error: To squelch this message and still keep the default behaviour, set
+		remote: error: 'receive.denyCurrentBranch' configuration variable to 'refuse'.
+		To git.doctor-doom.com:./repos/ServletFilter.git
+...^^it's resolvable in the following way *AND ALSO ASSUMING* that the original upstream 
+repo's config file holds nothing special (which i *think* is the common case):
+$ git clone --bare [--no-hardlinks --verbose] /path/to/repo  [/path/to/new/repo]
+__envHEREDOC__
+}
 helpgitsvn(){
 cat <<'__envHEREDOC__'
 Creates (new git repo) folder: trunk
